b739de797624731f896999121bcac9bd60afda00
sunnivmr <sunnivmr@stud.ntnu.no>
2020-02-24 15:55:20 +0100

    Added files from seminario

---
+++ grafo.js
@@ -0,0 +1,108 @@
+/*
+** Seminario 1: Grafo de escena
+** @autor: Sunniva Mathea Runde
+*/
+
+// Variables globales estandar
+var renderer, scene, camera;
+
+// Otras variables
+var angulo = 0;
+var esfera;
+var conjunto;
+
+init();
+loadScene();
+render();
+
+function init() {
+	// Funcion de init de motor, escena y camara
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(new THREE.Color(0x000000));
+	document.getElementById("container").appendChild(renderer.domElement);
+
+	// Escena
+	scene = new THREE.Scene();
+
+	// Camara
+	var aspectRatio = window.innerWidth/window.innerHeight;
+	camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+	camera.position.set(0.5, 2, 5);
+	camera.lookAt(new THREE.Vector3(0,0,0));
+
+}
+
+function loadScene() {
+	// Construye el grado de escena
+	// - Objetos (geometria, material)
+	// - Transformaciones
+	// - Organizar el grafo
+
+	// Objeto contenedor de cubo y esfera
+	conjunto = new THREE.Object3D();
+	conjunto.position.y = 1;
+
+
+	// Cubo
+	var geoCubo = new THREE.BoxGeometry(2, 2, 2);
+	var geoMat = new THREE.MeshBasicMaterial({color: 'green', wireframe: true});
+	var cubo = new THREE.Mesh(geoCubo, geoMat);
+	cubo.position.x = 2;
+
+	// Esfera
+	var geoEsfera = new THREE.SphereGeometry(1, 30, 30);
+	var material = new THREE.MeshBasicMaterial({color: "yellow", wireframe: true});
+	esfera = new THREE.Mesh(geoEsfera, material);
+
+	// Suelo
+	var geoSuelo = new THREE.PlaneGeometry(10, 10, 12, 12);
+	var matSuelo = new THREE.MeshBasicMaterial({color: 'grey', wireframe: false});
+	var suelo = new THREE.Mesh(geoSuelo, matSuelo);
+	suelo.rotation.x = -Math.PI/2;
+	suelo.position.y = -0.1;
+
+	// Objeto importado
+	var loader = new THREE.ObjectLoader();
+	loader.load('models/mario/mario-sculpture.json',
+		function (objeto) {
+			objeto.scale.set(0.01, 0.01, 0.01);
+			objeto.rotation.y = Math.PI/2;
+			cubo.add(objeto);
+		})
+
+	// Texto
+	var fontLoader = new THREE.FontLoader();
+	fontLoader.load('font/gentilis_bold.typeface.json',
+					function (font) {
+						var geoTextio = new THREE.TextGeometry('MARIO',
+							{
+								size: 0.5,
+								height: 0.1,
+								curveSegments: 3,
+								style: "normal",
+								font: font,
+								bevelThickness: 0.05,
+								bevelSize: 0.04,
+								bevelEnabled: true
+							});
+						var matTexto = new THREE.MeshBasicMaterial({color: red});
+						var texto = new THREE.Mesh(geoTexto, matTexto);
+						scene.add(texto);
+					});
+
+	// Grafo
+	scene.add(esfera);
+	scene.add(cubo);
+	scene.add(conjunto);
+	scene.add(new THREE.AxesHelper(3));
+	scene.add(suelo);
+
+}
+
+function render() {
+	// Blucle de refresco
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+}
